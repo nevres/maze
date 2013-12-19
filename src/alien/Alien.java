ac6c783fcd79a008e9804ed8871c6b3036eb0a90
nevres <nevres_jahic@hotmail.com>
2013-12-19 11:41:19 +0100

    second modify

--- src/alien/Alien.java
+++ src/alien/Alien.java
@@ -14,6 +14,6 @@ public class Alien {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("this is code that is changed");
+        System.out.println("this is code thsats is changed");
     }
 }
